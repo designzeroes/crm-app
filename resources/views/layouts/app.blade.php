3ade2ff2f919f56b5cbe04a22dfc7bcdfce8938d
designzeroservices@gmail.com <designzeroservices@gmail.com>
2023-08-26 12:54:23 +0500

    working on layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,142 +1,35 @@
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style layout-menu-fixed"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="{{asset('assets')}}/"
-  data-template="vertical-menu-template-free"
->
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Dashboard - Analytics | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <meta name="description" content="" />
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{asset('assets')}}/img/favicon/favicon.ico" />
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            @include('layouts.navigation')
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
-
-    <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="{{asset('assets')}}/vendor/fonts/boxicons.css" />
-
-    <!-- Core CSS -->
-    <link rel="stylesheet" href="{{asset('assets')}}/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{asset('assets')}}/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="{{asset('assets')}}/css/demo.css" />
-
-    <!-- Vendors CSS -->
-    <link rel="stylesheet" href="{{asset('assets')}}/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-
-    <link rel="stylesheet" href="{{asset('assets')}}/vendor/libs/apex-charts/apex-charts.css" />
-
-    <!-- Page CSS -->
-
-    <!-- Helpers -->
-    <script src="{{asset('assets')}}/vendor/js/helpers.js"></script>
-
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="{{asset('assets')}}/js/config.js"></script>
-  </head>
-
-  <body>
-    <!-- Layout wrapper -->
-    <div class="layout-wrapper layout-content-navbar">
-      <div class="layout-container">
-        <!-- Menu -->
-
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white dark:bg-gray-800 shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
             <!-- Page Content -->
+            <main>
                 {{ $slot }}
-                <!-- Page Content -->
-
-            <!-- Footer -->
-            <footer class="content-footer footer bg-footer-theme">
-                <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-                  <div class="mb-2 mb-md-0">
-                    ©
-                    <script>
-                      document.write(new Date().getFullYear());
-                    </script>
-                    , made with ❤️ by
-                    <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
-                  </div>
-                  <div>
-                    <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
-                    <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
-  
-                    <a
-                      href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
-                      target="_blank"
-                      class="footer-link me-4"
-                      >Documentation</a
-                    >
-  
-                    <a
-                      href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
-                      target="_blank"
-                      class="footer-link me-4"
-                      >Support</a
-                    >
-                  </div>
-                </div>
-              </footer>
-              <!-- / Footer -->
-  
-              <div class="content-backdrop fade"></div>
-            </div>
-            <!-- Content wrapper -->
-          </div>
-          <!-- / Layout page -->
+            </main>
         </div>
-  
-        <!-- Overlay -->
-        <div class="layout-overlay layout-menu-toggle"></div>
-      </div>
-      <!-- / Layout wrapper -->
-  
-      <div class="buy-now">
-        <a
-          href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
-          target="_blank"
-          class="btn btn-danger btn-buy-now"
-          >Upgrade to Pro</a
-        >
-      </div>
-  
-      <!-- Core JS -->
-      <!-- build:js assets/vendor/js/core.js -->
-      <script src="{{asset('assets')}}/vendor/libs/jquery/jquery.js"></script>
-      <script src="{{asset('assets')}}/vendor/libs/popper/popper.js"></script>
-      <script src="{{asset('assets')}}/vendor/js/bootstrap.js"></script>
-      <script src="{{asset('assets')}}/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-  
-      <script src="{{asset('assets')}}/vendor/js/menu.js"></script>
-      <!-- endbuild -->
-  
-      <!-- Vendors JS -->
-      <script src="{{asset('assets')}}/vendor/libs/apex-charts/apexcharts.js"></script>
-  
-      <!-- Main JS -->
-      <script src="{{asset('assets')}}/js/main.js"></script>
-  
-      <!-- Page JS -->
-      <script src="{{asset('assets')}}/js/dashboards-analytics.js"></script>
-  
-      <!-- Place this tag in your head or just before your close body tag. -->
-      <script async defer src="https://buttons.github.io/buttons.js"></script>
     </body>
-  </html>
-  
+</html>
