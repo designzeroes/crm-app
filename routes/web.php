0d6b33b09cc49246b5bc1dd6d01c02df20ecb54f
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-08-28 23:45:43 -0700

    routes seeting up

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/wlc', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
