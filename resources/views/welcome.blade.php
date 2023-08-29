998d57702231c778dfaea1378b35f8679aaddd89
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-08-29 00:44:40 -0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,15 +13,22 @@
 
   <body>
     <!-- Layout wrapper -->
-    <div class="layout-wrapper layout-content-navbar text-right">
+    <div class="layout-wrapper layout-content-navbar ">
       <div class="layout-container">
         <!-- Menu -->
-        @if (Route::has('login'))
+      
+
+           <!-- Menu -->
+
+         <!-- Layout container -->
+        <div class="layout-page">
+            <!-- Navbar -->
+            @if (Route::has('login'))
 <div class="header-controls sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
     @auth
     <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
     @else
-    <a href="{{ route('lgin') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
     @if (Route::has('register'))
     <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
@@ -29,19 +36,23 @@
     @endauth
 </div>
 @endif
-
-           <!-- Menu -->
-
-         <!-- Layout container -->
-        <div class="layout-page">
-            <!-- Navbar -->
-   
                <!-- Navbar -->
 
              <!-- Content wrapper -->
           <div class="content-wrapper">
                <!-- Content -->     
-   
+   <h2>Jobs</h2>
+   <table>
+    <tr>
+        <th>position</th>
+        <th>experience</th>
+    </tr>
+    <tbody>
+        <tr>
+
+        </tr>
+    </tbody>
+   </table>
                <!-- Content --> 
             <!-- Footer -->
             <footer class="content-footer footer bg-footer-theme">
@@ -61,14 +72,6 @@
       </div>
       <!-- / Layout wrapper -->
   
-      <div class="buy-now">
-        <a
-          href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
-          target="_blank"
-          class="btn btn-danger btn-buy-now"
-          >Upgrade to Pro</a
-        >
-      </div>
   
       <!-- Core JS -->
       @include('includes.controlpanel.scripts')
