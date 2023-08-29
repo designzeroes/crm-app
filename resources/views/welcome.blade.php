58f214d978ef66812ed4bc54c9bf836d1ee47960
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-08-29 02:53:08 -0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,7 +112,7 @@
         </div>
     </div>
 </nav>
-<nav class="navbar navbar-expand-lg navbar-light h-5 ">
+<nav class="navbar navbar-expand-lg navbar-light h-2 ">
     <!-- <a class="navbar-brand" href="#">Freelancer Clone</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
