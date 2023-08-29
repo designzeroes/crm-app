bcecdf7fb861b00d2f5a160b7638a6e2bd3be56c
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-08-29 00:10:51 -0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,82 @@
             <div class="col-sm-7">
               <div class="card-body">
                 <h5 class="card-title text-primary">
-                     🎉
                 </h5>
+
+            @if (Route::has('login'))
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <nav
+class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+id="layout-navbar"
+>
+<div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+  <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+    <i class="bx bx-menu bx-sm"></i>
+  </a>
+</div>
+
+<div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+  <!-- Search -->
+  <div class="navbar-nav align-items-center">
+    <div class="nav-item d-flex align-items-center">
+      <i class="bx bx-search fs-4 lh-0"></i>
+      <input
+        type="text"
+        class="form-control border-0 shadow-none"
+        placeholder="Search..."
+        aria-label="Search..."
+      />
+    </div>
+  </div>
+  <!-- /Search -->
+
+  <ul class="navbar-nav flex-row align-items-center ms-auto">
+    <!-- Place this tag where you want the button to render. -->
+    <li class="nav-item lh-1 me-3">
+      <a
+        class="github-button"
+        href="https://github.com/themeselection/sneat-html-admin-template-free"
+        data-icon="octicon-star"
+        data-size="large"
+        data-show-count="true"
+        aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
+        >Star</a
+      >
+    </li>
+
+    <!-- User -->
+    <li class="nav-item navbar-dropdown dropdown-user dropdown">
+      <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+        <div class="avatar avatar-online">
+          <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+        </div>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-end">
+        <li>
+          <a class="dropdown-item" href="#">
+            <div class="d-flex">
+              <div class="flex-shrink-0 me-3">
+                <div class="avatar avatar-online">
+                  <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                </div>
+              </div>@auth
+    <div class="flex-grow-1">
+        <span class="fw-semibold d-block">{{ Auth::user()->name }}</span>
+        <small class="text-muted">Admin</small>
+    </div>
+@endauth
+
                 <p class="mb-4">
                   You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
                   your profile.
@@ -32,8 +106,7 @@
           </div>
         </div>
       </div>
-        <!--/ Transactions -->
-    </div>
+      
   </div>
   <!-- / Content -->
 @stop
