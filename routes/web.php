a21e016f50049ae2eb710cb5b540c0b642972855
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-08-28 23:09:00 -0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
+Route::get('/wlc', function () {
     return view('welcome');
 });
 
