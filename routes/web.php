7405f3415f3581f6a7d8716dfe4055e241ba6cce
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-08-28 22:58:04 -0700

    route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
+Route::get('/w', function () {
     return view('welcome');
 });
 
