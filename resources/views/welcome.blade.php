0fc61c5aeeed7cd832b99127874cf15defccfc5f
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-08-29 01:22:26 -0700

    p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,90 +1,91 @@
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style layout-menu-fixed"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="{{asset('assets')}}/"
-  data-template="vertical-menu-template-free"
->
-  <head>
-   @include('includes.controlpanel.head')
-  </head>
+<html lang="en">
+<head>
+    @include('includes.controlpanel.head')
+<style>
+    .dot-pattern-bg {
+    background-image: radial-gradient(circle, #000 5%, transparent 5%),
+                      radial-gradient(circle, #000 5%, transparent 5%);
+    background-size: 30px 30px;
+    background-position: 0 0, 15px 15px;
+}
 
-  <body class="bg-dots">
-    <!-- Layout wrapper -->
-    <div class="layout-wrapper layout-content-navbar ">
-      <div class="layout-container">
+</style>
+</head>
+<body class="bg-dots">
+
+<div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
         <!-- Menu -->
-      
+        <!-- Menu -->
 
-           <!-- Menu -->
-
-         <!-- Layout container -->
+        <!-- Layout container -->
         <div class="layout-page">
             <!-- Navbar -->
             @if (Route::has('login'))
-<div class="header-controls sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-    @auth
-    <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-    @else
-    <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 "style="float:right">Log in</a>
+                <div class="header-controls sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="btn btn-primary">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="btn btn-secondary">Log in</a>
 
-    @if (Route::has('register'))
-    <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"style="float:right">Register</a>
-    @endif
-    @endauth
-</div>
-@endif
-               <!-- Navbar -->
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="btn btn-info ml-2">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <!-- Navbar -->
 
-             <!-- Content wrapper -->
-             <div class="container-xxl">
-                    <div class="authentication-wrapper authentication-basic container-p-y">
-                  
-          <div class="content-wrapper mt-5">
-               <!-- Content -->     
-               <div class="card">
-                        <div class="card-body">
-                            
-                            <h3>Jobs</h3>
-                            <div class="app-brand justify-content-center">
-                            
-   <table>
-    
-   <tr>
-        <th>position</th>
-        <th>experience</th>
-    </tr>
-    <tbody>
-        <tr>
-<td>laravel dvlpr</td>
-<td>3 year</td>
-        </tr>
-    </tbody>
-   </table>
-               <!-- Content --> 
-            <!-- Footer -->
-            <footer class="content-footer footer bg-footer-theme">
-   
-              </footer>
-              <!-- / Footer -->
-  
-              <div class="content-backdrop fade"></div>
-            </div>
             <!-- Content wrapper -->
-          </div>
-          <!-- / Layout page -->
+            <div class="container-xxl">
+                <div class="authentication-wrapper authentication-basic container-p-y">
+                    <div class="content-wrapper mt-5">
+                        <!-- Content -->
+                        <div class="card">
+                            <div class="card-body">
+                                <h3>Jobs</h3>
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                        <tr>
+                                            <th>Position</th>
+                                            <th>Experience</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>Laravel Developer</td>
+                                            <td>3 years</td>
+                                        </tr>
+                                        <!-- Add more rows as needed -->
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Content -->
+                        <!-- Footer -->
+                        <footer class="content-footer footer bg-footer-theme"></footer>
+                        <!-- / Footer -->
+                        <div class="content-backdrop fade"></div>
+                    </div>
+                    <!-- Content wrapper -->
+                </div>
+            </div>
+            <!-- / Layout page -->
         </div>
-  
+
         <!-- Overlay -->
         <div class="layout-overlay layout-menu-toggle"></div>
-      </div>
-      <!-- / Layout wrapper -->
-  
-  
-      <!-- Core JS -->
-      @include('includes.controlpanel.scripts')
-    </body>
+    </div>
+    <!-- / Layout wrapper -->
+
+    <div class="buy-now">
+        <a href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/" target="_blank" class="btn btn-danger btn-buy-now">Upgrade to Pro</a>
+    </div>
+
+    <!-- Core JS -->
+    @include('includes.controlpanel.scripts')
+</body>
 </html>
-  
