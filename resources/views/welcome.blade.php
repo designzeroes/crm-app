5770e2719b9d2faa4e49386b50efa75fa9af873a
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-08-29 00:26:29 -0700

    ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,81 +1,22 @@
-@extends('layouts.guest')
-@include('layouts.app')
-@section('content')
-  <div class="container-xxl flex-grow-1 container-p-y">
-    <div class="row">
-      <div class="col-lg-8 mb-4 order-0">
-        <div class="card">
-          <div class="d-flex align-items-end row">
-            <div class="col-sm-7">
-              <div class="card-body">
-                <h5 class="card-title text-primary">
-                </h5>
-
-            <!-- @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif -->
-            <nav
-class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
-id="layout-navbar"
+<!DOCTYPE html>
+<html
+  lang="en"
+  class="light-style layout-menu-fixed"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="{{asset('assets')}}/"
+  data-template="vertical-menu-template-free"
 >
-<div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-  <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-    <i class="bx bx-menu bx-sm"></i>
-  </a>
-</div>
+  <head>
+   @include('includes.controlpanel.head')
+  </head>
 
-<div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-  <!-- Search -->
-  <div class="navbar-nav align-items-center">
-    <div class="nav-item d-flex align-items-center">
-      <i class="bx bx-search fs-4 lh-0"></i>
-      <input
-        type="text"
-        class="form-control border-0 shadow-none"
-        placeholder="Search..."
-        aria-label="Search..."
-      />
-    </div>
-  </div>
-  <!-- /Search -->
-
-  <ul class="navbar-nav flex-row align-items-center ms-auto">
-    <!-- Place this tag where you want the button to render. -->
-    <li class="nav-item lh-1 me-3">
-      <a
-        class="github-button"
-        href="https://github.com/themeselection/sneat-html-admin-template-free"
-        data-icon="octicon-star"
-        data-size="large"
-        data-show-count="true"
-        aria-label="Star themeselection/sneat-html-admin-template-free on GitHub"
-        >Star</a
-      >
-    </li>
-
-    <!-- User -->
-    <li class="nav-item navbar-dropdown dropdown-user dropdown">
-      <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-        <div class="avatar avatar-online">
-          <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-        </div>
-      </a>
-      <ul class="dropdown-menu dropdown-menu-end">
-        <li>
-          <a class="dropdown-item" href="#">
-            <div class="d-flex">
-              <div class="flex-shrink-0 me-3">
-              @if (Route::has('login'))
+  <body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+            @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
@@ -89,29 +30,48 @@ id="layout-navbar"
                 </div>
             @endif
 
-                <p class="mb-4">
-                  You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
-                  your profile.
-                </p>
+           <!-- Menu -->
 
-                <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
-              </div>
+         <!-- Layout container -->
+        <div class="layout-page">
+            <!-- Navbar -->
+   
+               <!-- Navbar -->
+
+             <!-- Content wrapper -->
+          <div class="content-wrapper">
+               <!-- Content -->     
+   
+               <!-- Content --> 
+            <!-- Footer -->
+            <footer class="content-footer footer bg-footer-theme">
+   
+              </footer>
+              <!-- / Footer -->
+  
+              <div class="content-backdrop fade"></div>
             </div>
-            <div class="col-sm-5 text-center text-sm-left">
-              <div class="card-body pb-0 px-0 px-md-4">
-                <img
-                  src="{{asset('assets')}}/img/illustrations/man-with-laptop-light.png"
-                  height="140"
-                  alt="View Badge User"
-                  data-app-dark-img="illustrations/man-with-laptop-dark.png"
-                  data-app-light-img="illustrations/man-with-laptop-light.png"
-                />
-              </div>
-            </div>
+            <!-- Content wrapper -->
           </div>
+          <!-- / Layout page -->
         </div>
+  
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
       </div>
-      
-  </div>
-  <!-- / Content -->
-@stop
+      <!-- / Layout wrapper -->
+  
+      <div class="buy-now">
+        <a
+          href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/"
+          target="_blank"
+          class="btn btn-danger btn-buy-now"
+          >Upgrade to Pro</a
+        >
+      </div>
+  
+      <!-- Core JS -->
+      @include('includes.controlpanel.scripts')
+    </body>
+</html>
+  
