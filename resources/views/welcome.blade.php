c6e3f282355579b0a7a059cd6e52871ce2505c60
Design Zeroes <142875327+designzeroes@users.noreply.github.com>
2023-09-02 10:26:34 +0400

    color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     @include('includes.controlpanel.head')
 <style>
     .bg{
-        background-color:#697a8d;
+        background-color:cyan;
     }
     .dot-pattern-bg {
     background-image: radial-gradient(circle, #000 5%, transparent 5%),
