c55ebd8cc6bebfef72eee01df5b2e5fa86eb195c
designzeroservices@gmail.com <designzeroservices@gmail.com>
2023-09-10 12:48:24 +0500

    a

--- resources/views/auth/candidate/login.blade.php
+++ resources/views/auth/candidate/login.blade.php
@@ -145,7 +145,7 @@
             
                             <p class="text-center">
                             <span>New on our platform?</span>
-                            <a href="{{route('register')}}">
+                            <a href="{{route('user-register')}}">
                                 <span>Create an account</span>
                             </a>
                             </p>
