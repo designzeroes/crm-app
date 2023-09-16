eabc196e85c4c83aeadcfdf1c1c47e3c3c933b66
designzeroservices@gmail.com <designzeroservices@gmail.com>
2023-09-16 10:39:46 +0500

    foreach fix

--- resources/views/pages/front/job/index.blade.php
+++ resources/views/pages/front/job/index.blade.php
@@ -130,7 +130,7 @@
                                         </div>
                                     </div>
                                 </div>
-                                @endforeacfa-flip-horizontal
+                                @endforeach
                             </div>
                         </div>
                         
